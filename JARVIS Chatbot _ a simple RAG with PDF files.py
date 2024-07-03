7b911ae970d34aa5b24ef044c42a22e73458e22c
Mr. Jack Tung <72214141+Mr-Jack-Tung@users.noreply.github.com>
2024-07-03 22:33:07 +0700

    Update JARVIS Chatbot _ a simple RAG with PDF files.py

--- JARVIS Chatbot _ a simple RAG with PDF files.py
+++ JARVIS Chatbot _ a simple RAG with PDF files.py
@@ -30,7 +30,6 @@ from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import OllamaEmbeddings
 
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 
 from langchain_community.document_loaders import PyPDFLoader
