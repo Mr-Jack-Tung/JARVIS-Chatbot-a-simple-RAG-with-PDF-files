d3a16dececbbe441a3e30208d4cba3716b6f2e94
Mr. Jack Tung <72214141+Mr-Jack-Tung@users.noreply.github.com>
2024-09-19 10:15:44 +0700

    Update JARVIS_assistant.py

--- JARVIS_assistant.py
+++ JARVIS_assistant.py
@@ -5,41 +5,6 @@
 # Version: 0.1.5
 # Date: 18 December 2024 - 10 PM
 
-
-import os
-
-print("\npython -m pip install --upgrade pip")
-os.system('python -m pip install --upgrade pip')
-
-# Delelte Chroma vectorstore ------------------------------------------------------------
-import os.path
-import shutil
-
-path_to_folder = "chroma_vectorstore"
-folder_exists = os.path.exists(path_to_folder)
-
-if folder_exists:
-	shutil.rmtree(path_to_folder)
-	print("\n",path_to_folder,"is deleted")
-
-
-# Install needed packages ------------------------------------------------------------
-print("\npip install -Ur requirements.txt")
-os.system("pip install -Ur requirements.txt")
-
-# # Pull ollama Qwen2-7B model ------------------------------------------------------------
-import ollama
-
-print("\nollama pull nomic-embed-text") # Nomic Embed v1.5 Embedding
-ollama.pull('nomic-embed-text')
-
-# print("\nollama pull chroma/all-minilm-l6-v2-f32")
-# ollama.pull('chroma/all-minilm-l6-v2-f32')
-
-print("\nollama pull qwen2\n")
-ollama.pull('qwen2')
-
-
 from jarvis.gui import JARVIS_assistant
 
 if __name__ == "__main__":
